d267ced548928bd1c6cf3e29f34cf0fa63538b3b
Khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2023-07-25 13:01:10 +0200

    add homeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    HomeComponent,
+    WelcomePageComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
